85c22f31c97670ebedaa3e3023062d00691bd6ba
las5h4 <loganasmalley@gmail.com>
2020-10-01 14:51:14 -0500

    Basic Solution(no bonus missions)

--- counting-characters/Program.cs
+++ counting-characters/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace counting_characters
 {
@@ -6,7 +8,28 @@ namespace counting_characters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+
+            foreach (char c in loremIpsum)
+            {
+                if (!counts.Keys.Contains(c))
+                {
+                    counts.Add(c, 1);
+                }
+                else
+                {
+                    counts[c]++;
+                }
+            }
+
+            foreach (KeyValuePair<char, int> count in counts)
+            {
+                Console.WriteLine(count.Key + ": " + count.Value);
+            }
+
+            Console.ReadLine();
         }
     }
 }
