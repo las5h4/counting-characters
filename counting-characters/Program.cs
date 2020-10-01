52ef62a8e19c803c0595cf2d6af7c2df8f917c7e
las5h4 <loganasmalley@gmail.com>
2020-10-01 18:52:47 -0500

    super-bonus-mission

--- counting-characters/Program.cs
+++ counting-characters/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace counting_characters
 {
@@ -6,7 +9,32 @@ namespace counting_characters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            Console.WriteLine("Enter word(s) for character count");
+
+            string input = Console.ReadLine().ToUpper();
+            input = Regex.Replace(input, "[^a-zA-Z]", String.Empty);
+
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+
+            foreach (char c in input)
+            {
+                if (!counts.Keys.Contains(c))
+                {
+                    counts.Add(c, 1);
+                }
+                else
+                {
+                    counts[c]++;
+                }
+            }
+
+            foreach (KeyValuePair<char, int> count in counts)
+            {
+                Console.WriteLine(count.Key + ": " + count.Value);
+            }
+
+            Console.ReadLine();
         }
     }
 }
